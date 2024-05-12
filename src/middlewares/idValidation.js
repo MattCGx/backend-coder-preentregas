54a49e6182c5e33736c763a1b9fbcdd56ed2a295
MattCG <mattcontrerasg@gmail.com>
2024-05-12 18:30:09 -0300

    fix id validation

--- src/middlewares/idValidation.js
+++ src/middlewares/idValidation.js
@@ -1,3 +1,3 @@
 export const idValidation = (req, res, next) => {
-    req.body.id === req.params.productId ? res.status(404).json({ msg: 'Product cannot change ID' }): next()
+    req.body.id !== req.params.productId ? res.status(404).json({ msg: 'Product cannot change ID' }): next()
 }

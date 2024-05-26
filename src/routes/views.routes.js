2db6e0fc6f9be81556f328f3d7799354ae420cd2
MattCG <mattcontrerasg@gmail.com>
2024-05-26 01:11:12 -0300

    agregada vista home

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,10 +1,24 @@
 import { Router } from "express";
+import { __dirname } from "../path.js";
+import ProductManager from "../managers/products.manager.js";
+
 
 const router = Router();
+const productManager = new ProductManager(`${__dirname}/db/products.json`);
 
-router.get("/", (req, res) => {
-    res.render('productsView')
-})
+
+
+
+router.get("/", async (req, res) => {
+    try {
+        const {limit} = req.query;
+        const products = await productManager.getProducts(limit);
+        res.render('home', { products })
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    });    
+
 
 
 
